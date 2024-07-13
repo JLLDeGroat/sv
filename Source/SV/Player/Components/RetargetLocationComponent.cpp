a39cb24be0a0ed9fbaf544da8352a9ed970aa94e
jlldegroat <jay.degroat@outlook.com>
2024-07-13 20:36:46 +0100

    update

--- Source/SV/Player/Components/RetargetLocationComponent.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "RetargetLocationComponent.h"
-
-URetargetLocationComponent::URetargetLocationComponent(const FObjectInitializer& ObjectInitializer)
-	: USphereComponent() {
-
-}
